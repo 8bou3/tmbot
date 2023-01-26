05fe80472c9793d0d869f30ca44c449146178341
Spirit <i8bou3@gmail.com>
2023-01-26 17:49:54 +0300

    Initial `npm` and `TypeScript`

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ *
+ * @name tmbot
+ * @author Spirit <i8bou3@gmail.com>
+ * @license MIT
+ * @copyright (c) 2023 Spirit
+ *
+ */
+
+import type { Client } from "discord.js"
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
+const client = new Client({
+  intents: []
+})
+
+client.login(process.env.TOKEN)
